2033fe47eabdb223115aa22e7c59a10ff03cf709
Arkendorf <amonafk@yahoo.com>
2017-08-31 20:05:26 -0400

    improved blood

--- combat.lua
+++ combat.lua
@@ -1,8 +1,22 @@
 function combat_load()
   weapons = {}
-  weapons[1] = {type = 2, baseDmg = 5, idealDist = 48, rangePenalty = -.04, cost = 1, speed = 3}
+  weapons[1] = {type = 2, baseDmg = 5, idealDist = 48, rangePenalty = -.04, cost = 1}
   weapons[2] = {type = 1, baseDmg = 1, idealDist = 48, rangePenalty = -.04, cost = 1}
   projectiles = {}
+  projectileTypes = {}
+  projectileTypes[1] = {ai = 1, speed = 10, img = laserImg}
+
+  projectileAIs = {}
+  projectileAIs[1] = function(v, dt)
+    if v.dir == nil then
+      v.dir = getDirection({x = v.x, y = v.y}, {x = v.dX, y = v.dY})
+    end
+    if v.speed == nil then
+      v.speed = projectileTypes[v.type].speed
+    end
+    v.x = v.x + v.speed*dt*60*math.cos(v.angle)
+    v.y = v.y + v.speed*dt*60*math.sin(v.angle)
+  end
 end
 
 function attack(a, b)
@@ -24,23 +38,26 @@ function hitscan(a, b) -- a is shooter, b is target, dmg is damage to deal
   local angle = getAngle({x = a.x, y = a.y}, {x = b.x, y = b.y})
   local xOffset, yOffset = (tileSize/2*math.cos(angle)), (tileSize/2*math.sin(angle))
   particles[#particles + 1] = {room = a.room, x = a.x+xOffset, y = a.y+yOffset, type = 1, z = 8, displayAngle = displayAngle}
-  particles[#particles + 1] = {room = a.room, x = b.x-xOffset, y = b.y-yOffset, type = 2, z = 8, displayAngle = displayAngle+math.pi}
+  for i = 1, math.floor(dmg) do
+    particles[#particles + 1] = {room = a.room, x = b.x-xOffset, y = b.y-yOffset, type = 2, z = 8, displayAngle = 0}
+  end
 end
 
 function projectile(a, b)
   local dmg = getDamage(a, b)
   b.health = b.health - dmg
 
-  local speed = weapons[a.weapon].speed
   local x1, y1 = coordToIso(a.x, a.y)
   local x2, y2 = coordToIso(b.x, b.y)
   local displayAngle = getAngle({x = x1, y = y1}, {x = x2, y = y2})
   local angle = getAngle({x = a.x, y = a.y}, {x = b.x, y = b.y})
   local xOffset, yOffset = (tileSize/2*math.cos(angle)), (tileSize/2*math.sin(angle))
 
-  projectiles[#projectiles + 1] = {room = a.room, x = a.x+xOffset, y = a.y+yOffset, z = 8, dX = b.x-xOffset, dY = b.y-yOffset, speed = speed, angle = angle, displayAngle = displayAngle}
+  projectiles[#projectiles + 1] = {room = a.room, x = a.x+xOffset, y = a.y+yOffset, z = 8, dX = b.x-xOffset, dY = b.y-yOffset, type = 1, angle = angle, displayAngle = displayAngle}
   particles[#particles + 1] = {room = a.room, x = a.x+xOffset, y = a.y+yOffset, type = 1, z = 8, displayAngle = displayAngle}
-  particles[#particles + 1] = {room = a.room, x = b.x-xOffset, y = b.y-yOffset, type = 2, z = 8, displayAngle = displayAngle+math.pi}
+  for i = 1, math.floor(dmg) do
+    particles[#particles + 1] = {room = a.room, x = b.x-xOffset, y = b.y-yOffset, type = 2, z = 8, displayAngle = 0}
+  end
 end
 
 function getDamage(a, b) -- entity a is attacking entity b
@@ -83,17 +100,17 @@ function combat_update(dt)
     end
   end
 
+  local removeNils = false
   for i, v in ipairs(projectiles) do
-    if v.dir == nil then
-      v.dir = getDirection({x = v.x, y = v.y}, {x = v.dX, y = v.dY})
-    end
-    v.x = v.x + v.speed*dt*60*math.cos(v.angle)
-    v.y = v.y + v.speed*dt*60*math.sin(v.angle)
+    projectileAIs[projectileTypes[v.type].ai](v, dt)
     if (v.dX - v.x)*v.dir.x <= 0 and (v.dY - v.y)*v.dir.y <= 0 then
       projectiles[i] = nil
+      removeNils = true
     end
   end
-  projectiles = removeNil(projectiles)
+  if removeNils == true then
+    projectiles = removeNil(projectiles)
+  end
 end
 
 
@@ -101,7 +118,11 @@ function queueProjectiles(room)
   for i, v in ipairs(projectiles) do
     if v.room == room then
       local x, y = coordToIso(v.x, v.y)
-      drawQueue[#drawQueue + 1] = {type = 2, img = laserImg, x = x+tileSize, y = y+tileSize/2, z = v.z, angle = v.displayAngle}
+      if projectileTypes[v.type].quad == nil then
+        drawQueue[#drawQueue + 1] = {type = 2, img = projectileTypes[v.type].img, x = x+tileSize, y = y+tileSize/2, z = v.z, angle = v.displayAngle}
+      else
+        drawQueue[#drawQueue + 1] = {type = 2, img = projectileTypes[v.type].img, quad = projectileTypes[v.type].quad, x = x+tileSize, y = y+tileSize/2, z = v.z, angle = v.displayAngle}
+      end
     end
   end
 end
--- particle.lua
+++ particle.lua
@@ -2,7 +2,7 @@ function particle_load()
   particles = {}
   particleTypes = {}
   particleTypes[1] = {ai = 1, speed = 10, maxFrame = 3, time = .3, img = muzzleFlashImg, quad = muzzleFlashQuad}
-  particleTypes[2] = {ai = 1, speed = 10, maxFrame = 3, time = .3, img = bloodImg, quad = bloodQuad}
+  particleTypes[2] = {ai = 2, zV = 2, xV = 4, yV = 4, time = 10, img = bloodImg, quad = bloodQuad}
   particleAIs = {}
 
   particleAIs[1] = function(v, dt)
@@ -10,6 +10,7 @@ function particle_load()
     if v.frame == nil then
       v.frame = 1
     end
+
     if v.maxFrame == nil then
       v.maxFrame = particleTypes[v.type].maxFrame
     end
@@ -25,25 +26,74 @@ function particle_load()
     if v.frame > v.maxFrame+1 then
       v.frame = 1
     end
-    v.time = v.time - dt
+  end
+
+  particleAIs[2] = function(v, dt)
+    if v.frame == nil then
+      v.frame = 1
+    end
+    if v.move == nil then
+      v.move = true
+    end
+
+    if v.zV == nil then
+      v.zV = particleTypes[v.type].zV
+    end
+    if v.xV == nil then
+      v.xV = math.random(-particleTypes[v.type].xV, particleTypes[v.type].xV)
+    end
+    if v.yV == nil then
+      v.yV = math.random(-particleTypes[v.type].yV, particleTypes[v.type].yV)
+    end
+
+    if v.move == true then
+      v.z = v.z + v.zV
+      v.zV = v.zV - 0.2*dt*60
+      v.x = v.x + v.xV
+      v.xV = v.xV * 0.9
+      v.y = v.y + v.yV
+      v.yV = v.yV * 0.9
+
+      local x, y = coordToTile(v.x, v.y)
+      if v.z < 0 then
+        v.move = false
+        v.frame = 2
+      elseif x >= 1 and x <= #rooms[v.room][1] and y >= 1 and y <= #rooms[v.room] and tileType[rooms[v.room][y][x]] ~= 1 then
+        v.move = false
+        v.frame = 3
+      end
+    end
   end
 end
 
 function particle_update(dt)
+  local removeNils = false
   for i, v in ipairs(particles) do
     particleAIs[particleTypes[v.type].ai](v, dt)
+    if v.time == nil then
+      v.time = particleTypes[v.type].time
+    end
+    v.time = v.time - dt
     if v.time <= 0 then
       particles[i] = nil
+      removeNils = true
     end
   end
-  particles = removeNil(particles)
+  if removeNils == true then
+    particles = removeNil(particles)
+  end
 end
 
 function queueParticles(room)
   for i, v in ipairs(particles) do
     if v.room == room then
       local x, y = coordToIso(v.x, v.y)
-      drawQueue[#drawQueue + 1] = {type = 2, img = particleTypes[v.type].img, quad = particleTypes[v.type].quad[math.floor(v.frame)], x = x + tileSize, y = y+tileSize/2, z = v.z, angle = v.displayAngle}
+
+      if particleTypes[v.type].quad == nil then
+        drawQueue[#drawQueue + 1] = {type = 2, img = particleTypes[v.type].img, x = x + tileSize, y = y+tileSize/2, z = v.z, angle = v.displayAngle}
+      else
+        drawQueue[#drawQueue + 1] = {type = 2, img = particleTypes[v.type].img, quad = particleTypes[v.type].quad[math.floor(v.frame)], x = x + tileSize, y = y+tileSize/2, z = v.z, angle = v.displayAngle}
+      end
     end
   end
 end
