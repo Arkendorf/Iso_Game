6605b03a0c59658b96a25b7bf163f190f81f6d30
Arkendorf <amonafk@yahoo.com>
2017-09-05 08:14:50 -0400

    finished previous improvements

--- particle.lua
+++ particle.lua
@@ -1,25 +1,13 @@
 function particle_load()
   particles = {}
-  particles[1] = {ai = 1, speed = 10, maxFrame = 3, time = .3, img = muzzleFlashImg, quad = muzzleFlashQuad}
-  particles[2] = {ai = 2, zV = 2, xV = 3, yV = 3, time = 10, img = bloodImg, quad = bloodQuad}
-  
+  particles[1] = {ai = 1, startAI = 1, time = .3, speed = 10, maxFrame = 3, img = muzzleFlashImg, quad = muzzleFlashQuad}
+  particles[2] = {ai = 2, startAI = 2, time = 10, zV = 2, xV = 3, yV = 3, img = bloodImg, quad = bloodQuad}
+
   particleEntities = {}
 
   particleAIs = {}
 
   particleAIs[1] = function(v, dt)
-    -- set things to default if unspecified
-    if v.maxFrame == nil then
-      v.maxFrame = particles[v.type].maxFrame
-    end
-    if v.speed == nil then
-      v.speed = particles[v.type].speed
-    end
-    if v.time == nil then
-      v.time = particles[v.type].time
-    end
-
-    -- ai stuff
     v.frame = v.frame + dt * v.speed
     if v.frame > v.maxFrame+1 then
       v.frame = 1
@@ -27,20 +15,6 @@ function particle_load()
   end
 
   particleAIs[2] = function(v, dt)
-    if v.move == nil then
-      v.move = true
-    end
-
-    if v.zV == nil then
-      v.zV = particles[v.type].zV
-    end
-    if v.xV == nil then
-      v.xV = math.random(-particles[v.type].xV*10, particles[v.type].xV*10)/10
-    end
-    if v.yV == nil then
-      v.yV = math.random(-particles[v.type].yV*10, particles[v.type].yV*10)/10
-    end
-
     if v.move == true then
       v.z = v.z + v.zV
       v.zV = v.zV - 0.2*dt*60
@@ -53,27 +27,34 @@ function particle_load()
       if v.z < 0 then
         v.move = false
         v.frame = 2
-      elseif x >= 1 and x <= #rooms[v.room][1] and y >= 1 and y <= #rooms[v.room] and tileType[rooms[v.room][y][x]] ~= 1 then
-        v.move = false
-        v.frame = 3
+      elseif x >= 1 and x <= #rooms[v.room][1] and y >= 1 and y <= #rooms[v.room] then
+        if tileType[rooms[v.room][y][x]] ~= 1 then
+          v.move = false
+          v.frame = 3
+        end
       end
     end
     v.alpha = (v.time/particles[v.type].time)*255
   end
+
+  particleStartAIs = {}
+
+  particleStartAIs[1] = function (v)
+    v.speed = particles[v.type].speed
+    v.maxFrame = particles[v.type].maxFrame
+  end
+
+  particleStartAIs[2] = function (v)
+    v.move = true
+    v.zV = particles[v.type].zV
+    v.xV = math.random(-particles[v.type].xV*10, particles[v.type].xV*10)/10
+    v.yV = math.random(-particles[v.type].yV*10, particles[v.type].yV*10)/10
+  end
 end
 
 function newParticle(room, x, y, z, type, displayAngle, time, frame, alpha)
-  local i = #particleEntities + 1
-  particleEntities[i] = {room = room, x = x, y = y, type = type, z = z, displayAngle = displayAngle, time = time, frame = frame, alpha = alpha}
-  if alpha == nil then
-    particleEntities[i].alpha = 255
-  end
-  if time == nil then
-    particleEntities[i].time = particles[particleEntities[i].type].time
-  end
-  if frame == nil then
-    particleEntities[i].frame = 1
-  end
+  particleEntities[#particleEntities + 1] = {room = room, x = x, y = y, type = type, z = z, displayAngle = displayAngle, time = particles[type].time, frame = 1, alpha = 255}
+  particleStartAIs[particles[type].startAI](particleEntities[#particleEntities])
 end
 
 function particle_update(dt)
