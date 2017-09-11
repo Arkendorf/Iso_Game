2e562ebcdaa883a59c6e67ccb8059764771e2590
Arkendorf <amonafk@yahoo.com>
2017-09-10 20:03:31 -0400

    beginning of shader

--- graphics.lua
+++ graphics.lua
@@ -62,6 +62,20 @@ function graphics_load()
   goopQuad = createSpriteSheet(goopImg, 3, 1, 16, 16)
 
   laserImg = love.graphics.newImage("laser.png")
+
+  myShader = love.graphics.newShader[[
+    vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords ){
+      vec4 pixel = Texel(texture, texture_coords);//This is the current pixel color
+      if(pixel.a == 1.0 && screen_coords.y > 0){
+        return vec4(1.0,1.0,1.0,1.0);
+      }
+      else
+      {
+        return vec4(1.0,1.0,1.0,0.0);
+      }
+    }
+  ]]
+  love.graphics.setShader(myShader)
 end
 
 
